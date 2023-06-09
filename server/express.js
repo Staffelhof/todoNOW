b090f4941e52df604d217290dbeb1d790ea4b2cc
sibgatullin-i <67420207+sibgatullin-i@users.noreply.github.com>
2023-06-09 18:38:03 +0300

    delete note

--- server/express.js
+++ server/express.js
@@ -52,7 +52,7 @@ app.patch('/note/:id', (req, res) => {
     note: req.body.note,
   };
   notesDB.update(updatedNote, noteId).then((note) => {
-    if (note > 0) {
+    if (note > 0) { // this doesn't work :(
       res.json({ message: 'note updated' });
     } else {
       res.status(404).json({ error: 'not found' });
@@ -61,6 +61,16 @@ app.patch('/note/:id', (req, res) => {
     res.status(500).json({ error: err });
   });
 });
+
+// delete note by id, delete request to http://localhost:9000/note/id
+// curl -X DELETE http://localhost:9000/note/id
+app.delete('/note/:id', (req, res) => {
+  notesDB.delete(req.params.id).then((note) => {
+    res.json({ message: 'deleted' });
+  }, (err) => {
+    res.status(500).json({ error: err });
+  })
+})
 
 app.use('*', (req, res) => {
 // читаем файл `index.html`
